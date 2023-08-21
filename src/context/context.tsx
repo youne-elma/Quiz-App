3c159b3bd004c400011169351f71dbfd2d1d0940
younelma <youne.elma@gmail.com>
2023-08-21 15:50:21 +0100

    add the gamePage logic + update the favicon.png

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, createContext, useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 interface Question {
   category: string;
@@ -12,6 +13,8 @@ interface Question {
 interface DataContextType {
   questions: Question[];
   loading: boolean;
+  correctAnswers: string[];
+  buttonArray: ButtonArrayType[];
 }
 
 interface DataProviderProps {
@@ -22,11 +25,32 @@ export const DataContext = createContext<DataContextType | undefined>(
   undefined
 );
 
+interface Answer {
+  id: string;
+  index: number;
+  isCorrect: boolean;
+  name: string;
+}
+
+interface ButtonArrayType {
+  question: string;
+  answers: Answer[];
+}
+
 export const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [loading, setLoading] = useState(false);
+  const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
+  const [buttonArray, setButtonArray] = useState<ButtonArrayType[]>([]);
 
   const url = "https://opentdb.com/api.php?amount=5";
+
+  const shuffle = (array: Answer[]) => {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
 
   const pullData = async () => {
     setLoading(true);
@@ -43,15 +67,52 @@ export const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
       setLoading(false);
       console.error("Caught Error:", error);
     }
-    console.log("test");
   };
 
   useEffect(() => {
     pullData();
   }, [url]);
 
+  useEffect(() => {
+    const bigArray: ButtonArrayType[] = [];
+    const correctAnswerArray: string[] = [];
+    questions.map((item, index) => {
+      const correctAnswers1 = item.correct_answer;
+      correctAnswerArray.push(correctAnswers1);
+      setCorrectAnswers(correctAnswerArray);
+      const wrongAnswers = item.incorrect_answers;
+      const wrongAnswersArray = wrongAnswers.map((temp) => ({
+        name: temp,
+        isCorrect: false,
+        id: nanoid(),
+        index,
+      }));
+      const correctAnswer = {
+        name: item.correct_answer,
+        isCorrect: true,
+        id: nanoid(),
+        index,
+      };
+      wrongAnswersArray.push(correctAnswer);
+
+      console.log(wrongAnswersArray);
+
+      shuffle(wrongAnswersArray);
+      const questionObject = {
+        question: item.question,
+        answers: wrongAnswersArray,
+      };
+      return bigArray.push(questionObject);
+    });
+    setButtonArray(bigArray);
+    // console.log(bigArray);
+    // console.log(correctAnswerArray + "correctAnswerArray");
+  }, [questions]);
+
   return (
-    <DataContext.Provider value={{ questions, loading }}>
+    <DataContext.Provider
+      value={{ questions, loading, buttonArray, correctAnswers }}
+    >
       {children}
     </DataContext.Provider>
   );
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
 import { useData } from "../../context/useDataContext";
+import { Link } from "react-router-dom";
+import LoadingLayout from "../../components/Loading";
+import blop3 from "../../assets/blop3.png";
+import blop4 from "../../assets/blop4.png";
+
+import Question from "./Question";
+import ButtonGroups from "./ButtonGroups";
 
 interface Question {
   category: string;
@@ -10,13 +18,105 @@ interface Question {
 }
 
 function Game() {
-  const { questions } = useData();
+  const [message, setMessage] = useState<boolean>(false);
+  const initChoicesArray: string[] = ["", "", "", "", ""];
+  const [choices, setChoices] = useState<string[]>(initChoicesArray);
+  const initButtonGroupArray: number[] = [0, 0, 0, 0, 0];
+  const [buttonGroup, setButtonGroup] =
+    useState<number[]>(initButtonGroupArray);
+  const [answered, setAnswered] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(0);
+
+  const { loading, buttonArray, correctAnswers } = useData();
+
+  const quizQuestions = buttonArray.map((item) => {
+    const unicodeQ = item.question;
+    const diffQuestions = unicodeQ
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&amp;/g, "&")
+      .replace(/&rsquo;/g, "")
+      .replace(/&oacute;/g, "Ó")
+      .replace(/&uacute;/g, "ú")
+      .replace(/&eacute;/g, "é")
+      .replace(/&prime;/g, "`")
+      .replace(/&lsquo/g, "“")
+      .replace(/&rsquo/g, "”")
+      .replace(/&divide;/g, "/");
+    const answerButtons = item.answers.map((ans) => {
+      const unicodeA = ans.name;
+      const fixedAnswers = unicodeA
+        .replace(/&quot;/g, '"')
+        .replace(/&#039;/g, "'")
+        .replace(/&amp;/g, "&")
+        .replace(/&oacute;/g, "Ó")
+        .replace(/&uacute;/g, "ú")
+        .replace(/&eacute;/g, "é")
+        .replace(/&prime;/g, "`")
+        .replace(/&lsquo/g, "“")
+        .replace(/&rsquo/g, "”")
+        .replace(/&divide;/g, "/");
+      return (
+        <ButtonGroups
+          fixedAnswers={fixedAnswers}
+          key={ans.id}
+          index={ans.index}
+          isCorrect={ans.isCorrect}
+          answered={answered}
+          choices={choices}
+          setChoices={setChoices}
+          buttonGroup={buttonGroup}
+          setButtonGroup={setButtonGroup}
+          active={buttonGroup[ans.index] === ans.id}
+        />
+      );
+    });
+    return <Question questions={diffQuestions} answerButtons={answerButtons} />;
+  });
+
+  const checkAnswers = (correctResp: string[], choice: string[]) => {
+    setCount(0);
+    if (choice.includes("")) {
+      setMessage(true);
+    }
+    for (let i = 0; i < correctResp.length; i += 1) {
+      if (correctResp[i] === choice[i]) {
+        setCount((prevState) => prevState + 1);
+      } else if (!choice.includes("")) {
+        setAnswered(true);
+        setMessage(false);
+      }
+    }
+  };
 
   return (
-    <div>
-      {questions.map((question: Question) => (
-        <li key={question.question}>{question.question}</li>
-      ))}
+    <div className="main">
+      <img className="blop3" src={blop3} alt="pictureBlop" />
+      {loading && <LoadingLayout />}
+      <div className="questions">{quizQuestions}</div>
+      <div className="result">
+        {message && <p className="message">You must answer all questions</p>}
+        {!answered && (
+          <button
+            type="button"
+            className="checkAnswer"
+            onClick={() => checkAnswers(correctAnswers, choices)}
+          >
+            Check the answers
+          </button>
+        )}
+        {answered && (
+          <>
+            <p>You scored {count}/5 correct answers</p>
+            <Link to="/quiz-project/" className="playAgain">
+              <button type="button" className="checkAnswer">
+                Play Again
+              </button>
+            </Link>
+          </>
+        )}
+      </div>
+      <img className="blop4" src={blop4} alt="pictureBlop" />
     </div>
   );
 }
