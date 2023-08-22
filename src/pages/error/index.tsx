f56f9f4d4d010e781c9a337781c705facabea588
younelma <youne.elma@gmail.com>
2023-08-22 12:09:36 +0100

    Add the error page

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
+import gifblop1 from "../../assets/gifblop1.gif";
+import gifblop2 from "../../assets/gifblop2.gif";
+
 function Error() {
-  return <div>Error Page</div>;
+  return (
+    <div className="main start-game">
+      <img src={gifblop1} className="gifBlop1" alt="gif blop 1" />
+      <div className="main-text">
+        <h1 className="title-quiz">Error 404 page</h1>
+        <Link to="/quiz-project/" className="error-button">
+          Go back
+        </Link>
+      </div>
+      <img src={gifblop2} className="gifBlop2" alt="gif blop 2" />
+    </div>
+  );
 }
 
 export default Error;
