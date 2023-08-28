d3a917d6fc4025ef5683870c3dd244a48ba30bd6
younelma <youne.elma@gmail.com>
2023-08-28 16:50:10 +0100

    Feature: add the front form in the start-game page

--- src/pages/start-game/index.tsx
+++ src/pages/start-game/index.tsx
@@ -4,22 +4,66 @@ import blop1 from "../../assets/blop1.png";
 import blop2 from "../../assets/blop2.png";
 import spikeImage from "../../assets/spike-image.png";
 import volleyballIcon from "../../assets/volleyball-icon.png";
+import { useData } from "../../context/useDataContext";
 
 function StartGame() {
+  const { categoriesData } = useData();
+
   return (
     <div className="main start-game">
       <img className="blop1" src={blop1} alt="blop1" />
       <img className="blop2" src={blop2} alt="blop2" />
-      <div className="main-text">
+      <form className="main-text">
         <h1 className="title-quiz">Quiz project</h1>
         <p className="parag-quiz">
           a project quiz,
           <br /> hurry up and start the quiz
         </p>
-        <Link to="/Quiz-App/game" className="start-button">
-          start the quiz
-        </Link>
-      </div>
+        <div className="groupForm">
+          <div className="selectForm">
+            <label className="selectForm-text">Number of Questions:</label>
+            <input
+              className="selectTag"
+              type="number"
+              min="1"
+              id="questionsNumber"
+            />
+          </div>
+          <div className="selectForm">
+            <label className="selectForm-text">Select the difficulty:</label>
+            <select className="selectTag" name="category" id="category">
+              <option selected>Any Category</option>
+              <option>Easy</option>
+              <option>Medium</option>
+              <option>Hard</option>
+            </select>
+          </div>
+        </div>
+        <div className="groupForm">
+          <div className="selectForm">
+            <label className="selectForm-text">Select the Category:</label>
+            <select className="selectTag" name="category" id="category">
+              <option selected>Any Category</option>
+              {categoriesData.map((item) => {
+                return <option key={item.id}>{item.name}</option>;
+              })}
+            </select>
+          </div>
+          <div className="selectForm">
+            <label className="selectForm-text">Select the Type:</label>
+            <select className="selectTag" name="category" id="category">
+              <option selected>Any Type</option>
+              <option>Multiple Choice</option>
+              <option>True / False</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit" className="start-button" onClick={}>
+          <Link to="/Quiz-App/game" className="linkStartButton">
+            start the quiz
+          </Link>
+        </button>
+      </form>
       <img className="spike-image" src={spikeImage} alt="spike volleyball" />
       <img
         className="volleyball-icon"
